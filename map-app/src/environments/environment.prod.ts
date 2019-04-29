78f83f79887607c512df3cce9e4cca433902c27a
benjamindcosta <dcosta871@gmail.com>
2019-04-28 20:40:18 -0700

    Modify env url

--- map-app/src/environments/environment.prod.ts
+++ map-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true
 };
-export const FLASK_URL = 'http://ec2-35-167-243-5.us-west-2.compute.amazonaws.com';
+export const FLASK_URL = 'http://54.186.83.48';
--- map-app/src/environments/environment.ts
+++ map-app/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const FLASK_URL = 'http://ec2-34-216-57-198.us-west-2.compute.amazonaws.com';
+export const FLASK_URL = 'http://54.186.83.48';
 
 /*
  * For easier debugging in development mode, you can import the following file
